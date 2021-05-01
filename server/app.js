b7fa3cbb31b42dad72574e3ba0205b36f90b2398
kolodidon <www.teteva@yandex.ru>
2021-05-01 20:07:55 +0300

    initial commit

---
+++ server/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+const app = express();
+const PORT = process.env.port || 5000;
+
+mongoose.connect(`mongodb+srv://User-1:lhbvvb572@cluster0.lvpar.mongodb.net/GraphQL_Films_App_DB?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true })
+
+app.use(cors())
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true
+}));
+
+const dbConnection = mongoose.connection
+dbConnection.on('error', err => console.log(`Connection error: ${err}`))
+dbConnection.once('open', () => console.log('Connected to DB!'))
+
+app.listen(PORT, err => {
+  err ? console.log(err) : console.log('Server started!');
+});
